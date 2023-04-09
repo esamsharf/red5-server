c94d8d27e861d37048fecfcab43439ed0086528d
“esamsharf” <esamsharf2@gmail.com>
2023-04-09 21:57:26 +0200

    remove classes

--- io/src/main/java/org/red5/io/m4a/impl/BoxReader.java
+++
@@ -1,8 +0,0 @@
-package org.red5.io.m4a.impl;
-
-public class BoxReader {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-    
-}
--- io/src/main/java/org/red5/io/m4a/impl/FrameMetadataManager.java
+++
@@ -1,8 +0,0 @@
-package org.red5.io.m4a.impl;
-
-public class FrameMetadataManager {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-    
-}
