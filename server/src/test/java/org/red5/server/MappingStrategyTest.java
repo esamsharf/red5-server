09129eed2b56a5b370770491dd1e8291384a5b39
“esamsharf” <esamsharf2@gmail.com>
2023-04-10 02:10:51 +0200

    These tests verify the cases where the method receives an empty or null string.

--- server/src/test/java/org/red5/server/MappingStrategyTest.java
+++ server/src/test/java/org/red5/server/MappingStrategyTest.java
@@ -17,4 +17,74 @@ public class MappingStrategyTest {
         String result = strategy.mapResourcePrefix("/test");
         assertEquals("/test/", result);
     }
+
+    @Test
+    public void testMapResourcePrefixWithNull(){
+        MappingStrategy strategy = new MappingStrategy();
+        assertThrows(IllegalArgumentException.class, () -> {
+            strategy.mapResourcePrefix(null);
+        });
+    }
+
+    @Test
+    public void testMapServiceNameWithNull(){
+        MappingStrategy strategy = new MappingStrategy();
+        assertThrows(IllegalArgumentException.class, () -> {
+            strategy.mapServiceName(null);
+        });
+    }
+
+    @Test
+    public void testMapServiceNameWithRoot(){
+        MappingStrategy strategy = new MappingStrategy();
+        String result = strategy.mapServiceName("/");
+        assertEquals("default.service", result);
+    }
+
+    @Test
+    public void testMapServiceNameWithPath(){
+        MappingStrategy strategy = new MappingStrategy();
+        String result = strategy.mapServiceName("/test");
+        assertEquals("/test.service", result);
+    }
+
+    @Test
+    public void testMapHandlerNameWithNull(){
+        MappingStrategy strategy = new MappingStrategy();
+        assertThrows(IllegalArgumentException.class, () -> {
+            strategy.mapHandlerName(null);
+        });
+    }
+
+    @Test
+    public void testMapHandlerNameWithRoot(){
+        MappingStrategy strategy = new MappingStrategy();
+        String result = strategy.mapHandlerName("/");
+        assertEquals("default.handler", result);
+    }
+
+    @Test
+    public void testMapHandlerNameWithPath(){
+        MappingStrategy strategy = new MappingStrategy();
+        String result = strategy.mapHandlerName("/test");
+        assertEquals("/test.handler", result);
+    }
+
+    @Test
+    public void testSetDefaultAppWithNull(){
+        MappingStrategy strategy = new MappingStrategy();
+        assertThrows(IllegalArgumentException.class, () -> {
+            strategy.setDefaultApp(null);
+        });
+    }
+
+    @Test
+    public void testSetDefaultAppWithEmptyString(){
+        MappingStrategy strategy = new MappingStrategy();
+        assertThrows(IllegalArgumentException.class, () -> {
+            strategy.setDefaultApp("");
+        });
+    }
+
+    
 }
